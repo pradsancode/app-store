698d4c9b0b9c44d7cded04b1315544afe18d7783
PRADSAN <pradsancare@gmail.com>
2023-02-06 10:43:36 +0800

    Update link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-//function rubah(angka) {
-  //var reverse = angka.toString().split('').reverse().join(''),
-    //ribuan = reverse.match(/\d{1,3}/g);
-  //ribuan = ribuan.join('.').split('').reverse().join('');
-  //return ribuan;
-//}
-//document.getElementById("saldo_user").innerText = rubah(document.getElementById("saldo_user").textContent);
+// function rubah(angka) {
+//   var reverse = angka.toString().split('').reverse().join(''),
+//     ribuan = reverse.match(/\d{1,3}/g);
+//   ribuan = ribuan.join('.').split('').reverse().join('');
+//   return ribuan;
+// }
+// document.getElementById("saldo_user").innerText = rubah(document.getElementById("saldo_user").textContent);
 
 function beli(link) {
   var NomorTujuan = document.getElementById('nomor-tujuan').value;
